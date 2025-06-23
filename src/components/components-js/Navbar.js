d8da110a264325cc055ca7e5c9d6d5c6cbe43a96
Tsunokaway <tsunokaway@gmail.com>
2025-06-23 12:04:57 -0300

    site-responsivo

--- src/components/components-js/Navbar.js
+++ src/components/components-js/Navbar.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import '../components-css/Navbar.css'
-import Icon from '../images/icon.svg'
+import React from 'react';
+import '../components-css/Navbar.css';
 
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <div className="navbar">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-6" className='my-icon'>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
-                </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-6" className='my-icon'>
+                <path strokeLinecap="round" strokeLinejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
+            </svg>
             <div className="links">
                 <ul className="links-li">
                     <li><a href="#hero">Home</a></li>
